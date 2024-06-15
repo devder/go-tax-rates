4cba9fe65d872108ad67d566f426cad44cbddb74
hal <72111003+devder@users.noreply.github.com>
2024-06-15 10:07:11 +0100

    use defer to close files

--- file_manager/file_manager.go
+++ file_manager/file_manager.go
@@ -20,6 +20,8 @@ func (fm FileManager) ReadLines() ([]string, error) {
 		return nil, errors.New("failed to open file")
 	}
 
+	defer file.Close() // func is called after the ReadLines finishes
+
 	scanner := bufio.NewScanner(file)
 
 	var lines []string
@@ -33,11 +35,11 @@ func (fm FileManager) ReadLines() ([]string, error) {
 	err = scanner.Err()
 
 	if err != nil {
-		file.Close()
+		// file.Close()
 		return nil, errors.New("failed to read file content")
 	}
 
-	file.Close()
+	// file.Close()
 	return lines, nil
 
 }
@@ -49,17 +51,17 @@ func (fm FileManager) WriteResult(data any) error {
 		return errors.New("failed to create file")
 	}
 
+	defer file.Close()
+
 	time.Sleep(3 * time.Second)
 
 	encoder := json.NewEncoder(file)
 	err = encoder.Encode(data)
 
 	if err != nil {
-		file.Close()
 		return errors.New("failed to convert data to JSON")
 	}
 
-	file.Close()
 	return nil
 }
 
